200b3f113075eb3b401f04ae3eeddf12dfe1b5c6
Steeve Morin <steeve.morin@gmail.com>
2022-09-09 15:51:48 +0200

    packages/mediacenter/OutFox: add OutFox package
    Signed-off-by: Steeve Morin <steeve.morin@gmail.com>

---
+++ packages/mediacenter/OutFox/package.mk
@@ -0,0 +1,23 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="OutFox"
+PKG_VERSION="0.4.16.0"
+PKG_SHA256="f3bcde9bf0053839de999d36ce200f2f0bd63850f6808b263dbb56b092df7a35"
+PKG_LICENSE="GPL"
+PKG_SITE="https://projectoutfox.com/"
+PKG_URL="https://github.com/TeamRizu/OutFox/releases/download/OF4.16.0/OutFox-alpha-0.4.16.0HF2-Linux-amd64-legacy-date-20220801.tar.gz"
+PKG_TOOLCHAIN="manual"
+PKG_DEPENDS_TARGET="pulseaudio libjack"
+PKG_LONGDESC="Project OutFox is an extensible rhythm game engine based on StepMania, featuring 17 different modes simulating different dance, instrument, and key-based rhythm games."
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/opt/outfox
+  cp -rp * ${INSTALL}/opt/outfox/
+}
+
+post_install() {
+  enable_service outfox.target
+  enable_service outfox.service
+}
