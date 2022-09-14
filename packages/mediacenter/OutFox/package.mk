4fe767e4e4e70aeb417ce4aa9adadb7bcd1d1cab
Steeve Morin <steeve.morin@gmail.com>
2022-09-14 10:09:26 +0200

    packages/mediacenter/OutFox: add litboard support
    First of, the Sextet FIFO is handeld by systemd so that it the
    accompanying service ensures the FIFO as a reader (preventing failure to
    open the file) and that it is consumed, so that OutFox doesn't block
    should the reader be slow.
    
    The service then uses socat to forward the data to UDP datagrams on
    localhost:5001. Meaning that that data is lost if no liboard is plugged,
    but the FIFO is always consumed.
    
    On the other side, a udev rule on idVendor=1a86 idProduct=7523 will
    start a consuming service that will fetch datagrams from :5001 and
    forward them to the litboard, again using socat.
    
    This effectively brings plug and play support of the litboard, as OutFox
    is running.
    
    Signed-off-by: Steeve Morin <steeve.morin@gmail.com>

--- packages/mediacenter/OutFox/package.mk
+++ packages/mediacenter/OutFox/package.mk
@@ -9,7 +9,7 @@ PKG_LICENSE="GPL"
 PKG_SITE="https://projectoutfox.com/"
 PKG_URL="https://github.com/TeamRizu/OutFox/releases/download/OF4.16.0/OutFox-alpha-0.4.16.0HF2-Linux-amd64-legacy-date-20220801.tar.gz"
 PKG_TOOLCHAIN="manual"
-PKG_DEPENDS_TARGET="pulseaudio libjack"
+PKG_DEPENDS_TARGET="pulseaudio libjack socat"
 PKG_LONGDESC="Project OutFox is an extensible rhythm game engine based on StepMania, featuring 17 different modes simulating different dance, instrument, and key-based rhythm games."
 
 makeinstall_target() {
@@ -17,9 +17,15 @@ makeinstall_target() {
   cp -frp * ${INSTALL}/opt/outfox/
   rm -rf ${INSTALL}/opt/outfox/Songs/Outfox
   cp -fp ${PKG_DIR}/scripts/configure.sh ${INSTALL}/opt/outfox/
+
+  mkdir -p ${INSTALL}/opt/outfox/Data
+  ln -s /tmp/outfox/StepMania-Lights-SextetStream.out ${INSTALL}/opt/outfox/Data/StepMania-Lights-SextetStream.out
 }
 
 post_install() {
   enable_service outfox.target
   enable_service outfox.service
+  enable_service outfox-sextetstream.socket
+  enable_service outfox-sextetstream.service
+  enable_service litboard.service
 }
--- packages/mediacenter/OutFox/scripts/configure.sh
+++ packages/mediacenter/OutFox/scripts/configure.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
+readonly outfox_dir="/storage/.project-outfox"
+readonly outfox_tmp="/tmp/outfox"
+readonly outfox_preferences="${outfox_dir}/Save/Preferences.ini"
 
-# Ensure that options are never changed and properly set.
-readonly outfox_dir="${HOME}/.project-outfox"
-readonly pref_ini="${outfox_dir}/Save/Preferences.ini"
-mkdir -p $(dirname "${pref_ini}")
-touch "${pref_ini}"
-sed -e 's/^FullscreenIsBorderlessWindow=.*/FullscreenIsBorderlessWindow=0/g' \
+function preferences_file {
+    mkdir -p $(dirname "${outfox_preferences}")
+    touch "${outfox_preferences}"
+    echo "${outfox_preferences}"
+}
+
+# Setup the tmpdir for OutFox
+mkdir -p "${outfox_tmp}"
+
+# Logs go in tmpfs
+mkdir -p "${outfox_tmp}/Logs"
+ln -n -f -s "${outfox_tmp}/Logs" "${outfox_dir}/Logs"
+
+# Configure default preferences
+sed \
+    -e 's/^FullscreenIsBorderlessWindow=.*/FullscreenIsBorderlessWindow=0/g' \
+    -e 's/^LightsDriver=.*/LightsDriver=SextetStreamToFile/g' \
     -e 's/^ShowLoadingWindow=.*/ShowLoadingWindow=0/g' \
     -e 's/^ShowMouseCursor=.*/ShowMouseCursor=0/g' \
     -e 's/^Windowed=.*/Windowed=0/g' \
-    -i "${pref_ini}"
+    -i "$(preferences_file)"
 
 # Some soundcards may not be completely detected at startup. Make sure this is
 # the case.
-/usr/sbin/alsactl restore
+/usr/sbin/alsactl restore --force --ignore || true
