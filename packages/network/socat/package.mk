295652df72f595511b9b987edc3922b643e6d6c3
Steeve Morin <steeve.morin@gmail.com>
2022-09-30 13:53:25 +0200

    packages/network/socat: add package
    This is needed for litboard support.
    
    Signed-off-by: Steeve Morin <steeve.morin@gmail.com>

---
+++ packages/network/socat/package.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="socat"
+PKG_VERSION="1.7.4.3"
+PKG_SHA256="d697245144731423ddbbceacabbd29447089ea223e9a439b28f9ff90d0dd216e"
+PKG_LICENSE="GPLv2"
+PKG_SITE="http://www.dest-unreach.org/socat/"
+PKG_URL="http://www.dest-unreach.org/socat/download/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_HOST="autotools:host"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Multipurpose relay"
